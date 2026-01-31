d4b097c368c23cd68c6a35c8394d15a813e10469
Pedro Henriques <pedrohenriques@id.uff.br>
2026-01-31 13:56:07 -0300

    Update marineanalysis.py

--- marineanalysis.py
+++ marineanalysis.py
@@ -5,25 +5,13 @@ df = pd.read_csv("bounties.csv")
 df["bounty"] = pd.to_numeric(df["bounty"], errors="coerce") # como csv não tem tipo, transformamos em numeric. se não for número, será substituído como NaN
 
 bounty_por_tripulacao = (
-    df.groupby("tripulacao")["bounty"]
-      .sum()
-      .reset_index()
-      .sort_values(by="bounty", ascending=False)
-)
+    df.groupby("tripulacao")["bounty"].sum().reset_index().sort_values(by="bounty", ascending=False))
 
 bounty_medio = df["bounty"].mean()
 
-capitaes_perigosos = (
-    df.groupby("capitao")["bounty"]
-      .sum()
-      .reset_index()
-      .sort_values(by="bounty", ascending=False)
-)
+capitaes_perigosos = (df.groupby("capitao")["bounty"].sum().reset_index().sort_values(by="bounty", ascending=False))
 
-top_piratas = (
-    df.sort_values(by="bounty", ascending=False)
-      .head(3)[["pirata", "bounty"]]
-)
+top_piratas = (df.sort_values(by="bounty", ascending=False).head(3)[["pirata", "bounty"]])
 
 print("Total de bounty por tripulação:")
 print(bounty_por_tripulacao, "\n")
